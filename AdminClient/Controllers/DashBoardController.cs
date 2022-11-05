ae015a0cc927b9c2c8ddc7e3c4a1289fdb2a7568
Rahul Solanki <rahulsolanki259@gmail.com>
2022-11-05 13:46:25 +0530

    Maintain Session for user-role

--- AdminClient/Controllers/DashBoardController.cs
+++ AdminClient/Controllers/DashBoardController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AdminClient.AppHelper;
+using AdminClient.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -10,11 +12,29 @@ namespace AdminClient.Controllers
 {
     [CheckSession]
     public class DashBoardController : Controller
-    {       
+    {
         [HttpGet]
         public IActionResult Index()
         {
+            var defaultSchoolSession = HttpContext.Session.GetString("httpSchool");
+            var defaultAcYearSession = HttpContext.Session.GetString("httpAcYear");
+            if (defaultSchoolSession == null)
+            {
+                HttpContext.Session.SetString("httpSchool", "3");
+            }
+
+            if (defaultAcYearSession == null)
+            {
+                HttpContext.Session.SetString("httpAcYear", "1");
+            }
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SetChangeSchoolSession(SessionManager session)
+        {
+            HttpContext.Session.SetString(session.key, session.value);
+            return Json(HttpContext.Session.GetString(session.value));
         }
     }
 }
---
+++ AdminClient/ViewModels/CustomModelHelpers.cs
@@ -0,0 +1,8 @@
+﻿namespace AdminClient.ViewModels
+{
+    public class SessionManager
+    {
+        public string key { get; set; }
+        public string value { get; set; }
+    }
+}
